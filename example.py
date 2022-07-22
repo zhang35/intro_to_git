bdc89c738cc20acc028a9a8ad8a3e45a0e2a29c4
black-formatter <black-formatter@github.com>
2022-07-22 06:04:13 +0000

    Committing formatted changes

--- example.py
+++ example.py
@@ -1,3 +1,3 @@
 def git_opeation35gggg():
-     print("I am adding example.py file to the remote repository.")
-     print('zhang35')
+    print("I am adding example.py file to the remote repository.")
+    print("zhang35")
